cf370f40ea27cb19ed966bd6109b6bba01617e7a
virgilsisoe <virgilsisoe@gmail.com>
2021-11-10 13:05:34 -0500

    extension utility functions first commit

---
+++ src/utils.ts
@@ -0,0 +1,44 @@
+import * as vscode from "vscode";
+
+/**
+ * Get configuration property value.
+ *
+ * If property name is not found, throws an error.
+ *
+ * @param property - name of the configuration property to get.
+ * @returns - the value of the property.
+ */
+export function pepConfig(property: string): unknown {
+    const config = vscode.workspace.getConfiguration("pythonEasyPrint");
+    const subConfig = config.get(property);
+
+    if (typeof subConfig === "undefined") {
+        throw new Error(`Configuration: ${property} doesn't exist`);
+    }
+
+    return subConfig;
+}
+
+/**
+ * Insert python 2 statement for print and unicode.
+ *
+ * Insert at the beginning of the file 2 python statements that will enable the
+ * to use the python 3 print function and display an utf-8 character.
+ *
+ * `# coding: utf-8`
+ * `from __future__ import print_function`
+ *
+ * @returns void if command is called with no active text editor.
+ */
+export function initPrintPython2(): void {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
+
+    const startupStatement = "# coding: utf-8\nfrom __future__ import print_function\n";
+
+    editor.edit((editBuilder) => {
+        editBuilder.insert(new vscode.Position(0, 0), startupStatement);
+    });
+}
