abdc58691f0ff854bd0fa3ba460cd4ce2d24d23a
virgilsisoe <virgilsisoe@gmail.com>
2021-11-11 16:55:06 -0500

    new tests and some refactor

--- src/test/suite/print_statements.test.ts
+++ src/test/suite/print_statements.test.ts
@@ -4,10 +4,21 @@ import * as vscode from "vscode";
 import * as prints from "../../print_statements";
 import * as testUtils from "./test_utils";
 
-const demoFolder = testUtils.getDemoFolder();
-const demoFile = testUtils.getDemoFolder("demo_file.py");
+setup("Clean Demo files", async () => {
+    await testUtils.cleanSettings();
+    await testUtils.cleanDemoFile();
+});
 
-suite("Print Statements", async () => {
+teardown("Clean Demo files", async () => {
+    await testUtils.cleanSettings();
+    await testUtils.cleanDemoFile();
+});
+
+suiteSetup("Open demo file", async () => {
+    await testUtils.focusDemoFile();
+});
+
+suite("Print Statements template", async () => {
     test("Basic print", () => {
         const statement = prints.getPrintStatement("print");
         assert.strictEqual(statement, `print("${prints.symbol} {@} {text} :", {text})`);
@@ -38,61 +49,37 @@ suite("Print Statements", async () => {
             prints.getPrintStatement("test");
         }, Error);
     });
-
-    test("Invalid statement", () => {
-        assert.throws(() => {
-            prints.getPrintStatement("test");
-        }, Error);
-    });
 });
 
-// TODO: global hooks for cleaning settings before/after each suite
-
 suite("Construct statement", () => {
-    setup("Open demo file", async () => {
-        const document = await vscode.workspace.openTextDocument(demoFile);
-        await vscode.window.showTextDocument(document, { viewColumn: vscode.ViewColumn.One });
-
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const start = new vscode.Position(0, 0);
-            editor.selection = new vscode.Selection(start, start);
-        }
-    });
-
-    test("Basic statement", async () => {
+    test("Print statement", async () => {
         await testUtils.updateConfig("customizeLogMessage", "line: %l");
-        const replace = prints.constructStatement("name", "print");
+        const replace = prints.constructPrintStatement("name", "print");
         assert.strictEqual(replace, `print("${prints.symbol} line: 1 name :", name)`);
     });
 
     test("Type statement", async () => {
         await testUtils.updateConfig("customizeLogMessage", "line: %l");
-        const replace = prints.constructStatement("name", "type");
+        const replace = prints.constructPrintStatement("name", "type");
         assert.strictEqual(replace, `print("${prints.symbol} line: 1 name type :", type(name))`);
+    });
+
+    test("Dir statement", async () => {
+        await testUtils.updateConfig("customizeLogMessage", "%f");
+        const replace = prints.constructPrintStatement("name", "dir");
+        assert.strictEqual(replace, `print("${prints.symbol} demo_file.py name dir :", dir(name))`);
     });
 
     // XXX: shouldn't need to test other statements as they are identical to the one above
 
     test("Help statement", async () => {
         await testUtils.updateConfig("customizeLogMessage", "line: %l");
-        const replace = prints.constructStatement("name", "help");
+        const replace = prints.constructPrintStatement("name", "help");
         assert.strictEqual(replace, `help(name)`);
     });
 });
 
 suite("Placeholder Constructor", () => {
-    setup("Open demo file", async () => {
-        const document = await vscode.workspace.openTextDocument(demoFile);
-        await vscode.window.showTextDocument(document, { viewColumn: vscode.ViewColumn.One });
-
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const start = new vscode.Position(0, 0);
-            editor.selection = new vscode.Selection(start, start);
-        }
-    });
-
     test("No placeholders", async () => {
         await testUtils.updateConfig("customizeLogMessage", "debug");
         const placeholder = prints.convertPlaceholders();
@@ -110,11 +97,115 @@ suite("Placeholder Constructor", () => {
         const placeholder = prints.convertPlaceholders();
         assert.strictEqual(placeholder, "line: 1");
     });
+});
 
-    test("Replace %t for time.", async () => {
-        await testUtils.updateConfig("customizeLogMessage", "%t");
-        const placeholder = prints.convertPlaceholders();
-        // basic date time format: 00/00/0000T00:00:00AM
-        assert.match(placeholder, /\d{2}\/\d{2}\/\d{4}T\d{2}:\d{2}:\d{2}[AP]M/);
+suite("Get document text", () => {
+    setup("Write to file", async () => {
+        await testUtils.writeDemoFile({
+            line1: "import random",
+            line2: "\n\n",
+            line3: "name='virgil'",
+        });
+    });
+
+    test("First word under cursor on first line", async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const word = prints.getDocumentText(editor);
+            assert.strictEqual(word, "import");
+        }
+    });
+
+    test("Second word under cursor on first line", async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            // select first line
+            const firstLineStart = new vscode.Position(0, 7);
+            const firstLineEnd = editor.document.lineAt(0).range.end;
+
+            editor.selection = new vscode.Selection(firstLineStart, firstLineEnd);
+            const word = prints.getDocumentText(editor);
+            assert.strictEqual(word, "random");
+        }
+    });
+
+    test("Selected line1", async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            // select first line
+            const firstLineStart = editor.document.lineAt(0).range.start;
+            const firstLineEnd = editor.document.lineAt(0).range.end;
+            editor.selection = new vscode.Selection(firstLineStart, firstLineEnd);
+
+            const word = prints.getDocumentText(editor);
+            assert.strictEqual(word, "import random");
+        }
+    });
+
+    test("No word under cursor", async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            // select second line: empty line
+            const firstLineStart = editor.document.lineAt(1).range.start;
+            const firstLineEnd = editor.document.lineAt(1).range.end;
+            editor.selection = new vscode.Selection(firstLineStart, firstLineEnd);
+
+            const word = prints.getDocumentText(editor);
+            assert.strictEqual(word, "");
+        }
+    });
+});
+
+suite("Final Print Statement", () => {
+    setup("Write to file", async () => {
+        await testUtils.writeDemoFile({
+            line1: "import random",
+            line2: "\n\n",
+            line3: "name='virgil'",
+        });
+    });
+
+    test("Print Statement first word under cursor on first line", () => {
+        const statement = prints.executeCommand("print");
+        assert.strictEqual(statement, `print("${prints.symbol} import :", import)`);
+    });
+
+    test("Print Type Statement word under cursor", () => {
+        const statement = prints.executeCommand("type");
+        assert.strictEqual(statement, `print("${prints.symbol} import type :", type(import))`);
+    });
+
+    test("Help Statement word under cursor", () => {
+        const statement = prints.executeCommand("help");
+        assert.strictEqual(statement, "help(import)");
+    });
+
+    test("Selected line", async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            // select first line
+            const firstLineStart = editor.document.lineAt(0).range.start;
+            const firstLineEnd = editor.document.lineAt(0).range.end;
+            editor.selection = new vscode.Selection(firstLineStart, firstLineEnd);
+
+            const statement = prints.executeCommand("print");
+            assert.strictEqual(
+                statement,
+                `print("${prints.symbol} import random :", import random)`
+            );
+        }
+    });
+
+    test("No word under cursor", async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            // select second line: empty line
+            const firstLineStart = editor.document.lineAt(1).range.start;
+            const firstLineEnd = editor.document.lineAt(1).range.end;
+            editor.selection = new vscode.Selection(firstLineStart, firstLineEnd);
+
+            const statement = prints.executeCommand("print");
+            assert.strictEqual(typeof statement, "undefined");
+        }
     });
 });
