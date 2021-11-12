2955e56373a9a8977a7ad8db3ab28249ca3c78e2
virgilsisoe <virgilsisoe@gmail.com>
2021-11-11 22:56:54 -0500

    refactoring

--- src/document_parser.ts
+++ src/document_parser.ts
@@ -1,54 +1,72 @@
 import * as vscode from "vscode";
 import * as utils from "./utils";
 
-export function documentParser(action: string) {
+export function* documentParser(editor: vscode.TextEditor) {
+    const document = editor.document;
+
+    for (let line = 0; line <= document.lineCount - 1; ++line) {
+        const lineObj = document.lineAt(line);
+        const lineText = lineObj.text.trim();
+
+        const symbolMatch = new RegExp("print\\(['\"]" + utils.symbol);
+        if (!lineText.match(symbolMatch)) {
+            continue;
+        }
+
+        const startPos = new vscode.Position(line, lineObj.firstNonWhitespaceCharacterIndex);
+
+        yield {
+            text: lineText,
+            range: new vscode.Range(startPos, lineObj.range.end),
+            rangeToNewLine: lineObj.rangeIncludingLineBreak,
+        };
+    }
+}
+
+export function commentLines(editor: vscode.TextEditor) {
+    editor.edit((editBuilder) => {
+        for (const line of documentParser(editor)) {
+            if (!line.text.startsWith("#")) {
+                editBuilder.replace(line.range, `# ${line.text}`);
+            }
+        }
+    });
+}
+
+export function uncommentLines(editor: vscode.TextEditor) {
+    editor.edit((editBuilder) => {
+        for (const line of documentParser(editor)) {
+            editBuilder.replace(line.range, `${line.text.replace("# ", "").trim()} `);
+        }
+    });
+}
+
+export function deleteLines(editor: vscode.TextEditor) {
+    editor.edit((editBuilder) => {
+        for (const line of documentParser(editor)) {
+            editBuilder.delete(line.rangeToNewLine);
+        }
+    });
+}
+
+export function executeCommand(action: string) {
     const editor = vscode.window.activeTextEditor;
 
     if (!editor) {
         return;
     }
 
-    const document = editor.document;
-
-    editor.edit((editBuilder) => {
-        for (let line = 0; line <= document.lineCount - 1; ++line) {
-            const lineObj = document.lineAt(line);
-            const lineText = lineObj.text.trim();
-
-            const symbolMatch = new RegExp("print\\(['\"]" + utils.symbol);
-            if (!lineText.match(symbolMatch)) {
-                continue;
-            }
-
-            const firstChar = lineObj.firstNonWhitespaceCharacterIndex;
-
-            const startPos = new vscode.Position(line, firstChar);
-            const lineRange = new vscode.Range(startPos, lineObj.range.end);
-
-            switch (action) {
-                case "comment":
-                    // comment lines only if they are not already commented.
-                    if (!lineText.startsWith("#")) {
-                        editBuilder.replace(lineRange, `#${lineText} `);
-                    }
-                    break;
-                case "uncomment":
-                    editBuilder.replace(lineRange, `${lineText.replace("#", "")} `);
-                    break;
-                case "delete":
-                    editBuilder.delete(lineObj.rangeIncludingLineBreak);
-                    break;
-                default:
-                    break;
-            }
-        }
-    });
-}
-
-export function commentLines() {
-    console.log();
-}
-
-export function executeCommand(action: string) {
-    documentParser(action);
+    switch (action) {
+        case "comment":
+            commentLines(editor);
+            break;
+        case "uncomment":
+            uncommentLines(editor);
+            break;
+        case "delete":
+            deleteLines(editor);
+            break;
+        default:
+            break;
+    }
 }
