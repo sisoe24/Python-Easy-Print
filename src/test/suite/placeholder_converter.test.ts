1db3648dad3e707c661a9a3f859d678f0aff8ffc
virgilsisoe <virgilsisoe@gmail.com>
2021-11-18 13:34:50 -0500

    create suite and func name and added test

--- src/test/suite/placeholder_converter.test.ts
+++ src/test/suite/placeholder_converter.test.ts
@@ -33,35 +33,45 @@ suite("PlaceholderConverter", () => {
         await testUtils.cleanSettings();
     });
 
-    test("Get function name one level depth", async () => {
-        const editor = await testUtils.focusDemoFile(demoFile, 1);
+    suite("Get function name", () => {
+        test("Get function name one level depth", async () => {
+            const editor = await testUtils.focusDemoFile(demoFile, 1);
 
-        const placeholder = new prints.PlaceholdersConverter(editor);
-        assert.strictEqual(placeholder.getFuncName(), "wrapper");
-    });
+            const placeholder = new prints.PlaceholdersConverter(editor);
+            assert.strictEqual(placeholder.getFuncName(), "wrapper");
+        });
 
-    test("Get function name two level depth", async () => {
-        const editor = await testUtils.focusDemoFile(demoFile, 3);
-        const placeholder = new prints.PlaceholdersConverter(editor);
-        assert.strictEqual(placeholder.getFuncName(), "inner_func");
-    });
+        test("Get function name two level depth", async () => {
+            const editor = await testUtils.focusDemoFile(demoFile, 3);
+            const placeholder = new prints.PlaceholdersConverter(editor);
+            assert.strictEqual(placeholder.getFuncName(), "inner_func");
+        });
 
-    test("Get function return two level depth", async () => {
-        const editor = await testUtils.focusDemoFile(demoFile, 4);
+        test("Get function return two level depth", async () => {
+            const editor = await testUtils.focusDemoFile(demoFile, 4);
 
-        // when printing the return of a function, it will assume that the function call
-        // is the one above. although not exactly what I want, the test is mostly
-        // so I can remember.
-        const placeholder = new prints.PlaceholdersConverter(editor);
-        assert.strictEqual(placeholder.getFuncName(), "wrapper");
-    });
+            // when printing the return of a function, it will assume that the function call
+            // is the one above. although not exactly what I want, the test is mostly
+            // so I can remember.
+            const placeholder = new prints.PlaceholdersConverter(editor);
+            assert.strictEqual(placeholder.getFuncName(), "wrapper");
+        });
 
-    test("Get function name", async () => {
-        const editor = await testUtils.focusDemoFile(demoFile, 7, 4);
+        test("Get func name but should return nothing because is top level indentation", async () => {
+            const editor = await testUtils.focusDemoFile(demoFile, 7);
 
-        const placeholder = new prints.PlaceholdersConverter(editor);
-        assert.strictEqual(placeholder.getFuncName(), "");
-        assert.strictEqual(placeholder.convert("%F"), "");
+            const placeholder = new prints.PlaceholdersConverter(editor);
+            assert.strictEqual(placeholder.getFuncName(), "");
+            assert.strictEqual(placeholder.convert("%F"), "");
+        });
+
+        test("Get func name but should return nothing because parent is top level indentation", async () => {
+            const editor = await testUtils.focusDemoFile(demoFile, 8, 4);
+
+            const placeholder = new prints.PlaceholdersConverter(editor);
+            assert.strictEqual(placeholder.getFuncName(), "");
+            assert.strictEqual(placeholder.convert("%F"), "");
+        });
     });
 
     test("Get file name ", async () => {
