24cdbabcdd2fe1e0eef0bfdca84f99554af2cbee
virgilsisoe <virgilsisoe@gmail.com>
2021-11-18 12:59:23 -0500

    add optional argument for selection

--- src/test/suite/test_utils.ts
+++ src/test/suite/test_utils.ts
@@ -56,20 +56,27 @@ export async function cleanDemoFile(): Promise<void> {
  *
  * @param filename the name of a file to open.
  * @param line optional line number for the cursor to start at. Defaults to `0` which would be line `1`.
- * @param char optional character position for the cursor to start at when opening the file.
- * Defaults to `0`
+ * @param startChar optional position for the cursor to start at. Defaults to `0`
+ * @param endChar optional position for the cursor to end at. If bigger than startChar,
+ * will create a selection. Defaults to `0`
  */
 export async function focusDemoFile(
     filename: string,
     line = 0,
-    char = 0
+    startChar = 0,
+    endChar = 0
 ): Promise<vscode.TextEditor> {
     const filepath = path.join(demoPath, filename);
     const document = await vscode.workspace.openTextDocument(filepath);
 
-    const selection = new vscode.Position(line, char);
+    const startSelection = new vscode.Position(line, startChar);
+
+    let endSelection = null;
+    if (endChar) {
+        endSelection = new vscode.Position(line, endChar);
+    }
     const editor = await vscode.window.showTextDocument(document, {
-        selection: new vscode.Selection(selection, selection),
+        selection: new vscode.Selection(startSelection, endSelection || startSelection),
     });
 
     return editor;
