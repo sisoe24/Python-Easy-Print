bc8d432f449aaecea8ac8e5af67613d225c51f36
virgilsisoe <virgilsisoe@gmail.com>
2021-11-18 15:41:54 -0500

    docs

--- src/selected_text.ts
+++ src/selected_text.ts
@@ -3,6 +3,13 @@ import * as utils from "./utils";
 
 import { statementConstructor } from "./print_statements";
 
+/**
+ * Select text object class.
+ *
+ * The class deals with the selection that could be either manually from the user,
+ * or automatically from vscode hovering cursor position.
+ *
+ */
 export class SelectedText {
     selection: vscode.Selection;
     document: vscode.TextDocument;
@@ -13,8 +20,14 @@ export class SelectedText {
     lineText: string;
     lineNumber: number;
 
+    /**
+     * Init method to initialize the selection.
+     *
+     * @param editor Current active text editor.
+     */
     constructor(editor: vscode.TextEditor) {
         this.editor = editor;
+
         this.document = editor.document;
         this.selection = editor.selection;
 
@@ -22,6 +35,13 @@ export class SelectedText {
         this.lineText = this.editor.document.lineAt(this.lineNumber).text;
     }
 
+    /**
+     * Add extra match to the selected/hover word.
+     *
+     * @param lineRange range object to parse its text for the match.
+     * @param pattern regex pattern to match inside the range.
+     * @returns the match or `null` if no match is made.
+     */
     private addExtraMatch(lineRange: vscode.Range, pattern: RegExp): string | null {
         const lineText = this.editor.document.getText(lineRange);
         const match = pattern.exec(lineText);
@@ -32,37 +52,73 @@ export class SelectedText {
         return null;
     }
 
-    private includeParentCall(rangeUnderCursor: vscode.Range): string {
+    /**
+     * Include parent calls if any: `foo.bar.foo`.
+     *
+     * This method will activate only if settings `includeParentCall` is enabled.
+     *
+     * This will check if the selected/hover word has any parent objects by searching
+     * to the left of the word. If yes then it will return till the
+     * last valid parent.
+     *
+     * @param endChar the character position where to end the line parsing.
+     * @returns the chain of parents or an empty string if there are none.
+     */
+    private includeParentCall(endChar: number): string {
         let parentCall = "";
 
         if (utils.pepConfig("includeParentCall")) {
             const pattern = new RegExp(`(?:\\w+(?:\\(.*\\)|\\.)*)*${this.hoverWord}`);
 
-            const lineRange = new vscode.Range(
-                new vscode.Position(this.lineNumber, 0),
-                new vscode.Position(this.lineNumber, rangeUnderCursor.end.character)
-            );
+            const startPos = new vscode.Position(this.lineNumber, 0);
+            const endPos = new vscode.Position(this.lineNumber, endChar);
+
+            const lineRange = new vscode.Range(startPos, endPos);
 
             parentCall = this.addExtraMatch(lineRange, pattern) || "";
         }
         return parentCall;
     }
 
-    private includeFuncCall(rangeUnderCursor: vscode.Range): string {
+    /**
+     * Include function parentheses if any: `foo(x, y)`.
+     *
+     * This method will activate only if settings `includeParentheses` is enabled.
+     *
+     * This will check if the selected/hover word has an execution call by searching
+     * if there any parentheses to the right of the word. If yes, then it will return
+     * the parentheses with everything inside.
+     *
+     * @param startChar the character position from where to start the line parsing.
+     * @returns the function call parenthesis with arguments or an empty string
+     * no match was made.
+     */
+    private includeFuncCall(startChar: number): string {
         let funcCall = "";
         if (utils.pepConfig("includeParentheses")) {
             const pattern = new RegExp(`(?<=^${this.hoverWord})(\\(.*?\\))`);
 
-            const lineRange = new vscode.Range(
-                new vscode.Position(this.lineNumber, rangeUnderCursor.start.character),
-                new vscode.Position(this.lineNumber + 1, 0)
-            );
+            const startPos = new vscode.Position(this.lineNumber, startChar);
+
+            // to check the full line, we must go the the beginning of the next line.
+            const nextLine = this.lineNumber + 1;
+            const endPos = new vscode.Position(nextLine, 0);
+
+            const lineRange = new vscode.Range(startPos, endPos);
 
             funcCall = this.addExtraMatch(lineRange, pattern) || "";
         }
         return funcCall;
     }
 
+    /**
+     * Get the word and the cursor position.
+     *
+     * Before returning the text, will also check for the configuration settings
+     * and add to selection if settings require so.
+     *
+     * @returns the text under the cursor or `null` if no text is present.
+     */
     private textUnderCursor(): string | null {
         const rangeUnderCursor = this.document.getWordRangeAtPosition(this.selection.active);
 
@@ -70,22 +126,46 @@ export class SelectedText {
         if (rangeUnderCursor) {
             this.hoverWord = this.document.getText(rangeUnderCursor);
 
-            const parentCall = this.includeParentCall(rangeUnderCursor);
-            const funcCall = this.includeFuncCall(rangeUnderCursor);
+            const parentCall = this.includeParentCall(rangeUnderCursor.end.character);
+            const funcCall = this.includeFuncCall(rangeUnderCursor.start.character);
 
             return (parentCall || this.hoverWord) + funcCall;
         }
         return null;
     }
 
+    /**
+     * Get the selected text.
+     *
+     * Selected text could be the manual selection or the hover selection.
+     *
+     * @returns the selected word or `null` no selection.
+     */
     private getSelectedText(): string | null {
         return this.document.getText(this.selection) || this.textUnderCursor();
     }
 
-    hasCodeBlock() {
+    /**
+     * Check if line has an opening code block.
+     *
+     * A code block starts with a `(`, `[` or `{`. If any of those characters are
+     * matched, it assumed to be a code block.
+     *
+     * @returns true if code contains a code block, false otherwise.
+     */
+    hasCodeBlock(): boolean {
         return Boolean(this.lineText.match(/[{([]/));
     }
 
+    /**
+     * Get the text selection.
+     *
+     * If multipleStatements option is true, then the original selected text will
+     * be split into multiple statements and returned inside the array as different
+     * elements.
+     *
+     * @returns An array with the selected text, or `null` if no text was selected.
+     */
     text(): string[] | null {
         const text = this.getSelectedText();
 
