42bc3c61091aabf8896cc71fab41661a5205b281
virgilsisoe <virgilsisoe@gmail.com>
2021-11-19 21:08:13 -0500

    new tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,7 +1,12 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
+
+import * as path from "path";
+import { readdirSync, readFileSync } from "fs";
+
 import * as utils from "../../utils";
 import * as testUtils from "./test_utils";
+import { printCommands, logCommands, documentCommands } from "../../extension";
 
 suiteSetup("Clean settings", () => {
     testUtils.cleanSettings();
@@ -12,9 +17,34 @@ suiteTeardown("Clean settings", () => {
 });
 
 suite("Extension Settings", () => {
-    test("Test settings name", () => {
+    test("logging.customLogName type", () => {
+        const config = utils.pepConfig("logging.customLogName");
+        assert.strictEqual(typeof config, "string");
+    });
+
+    test("logging.useRepr type", () => {
+        const config = utils.pepConfig("logging.useRepr");
+        assert.strictEqual(typeof config, "boolean");
+    });
+
+    test("prints.addCustomMessage type", () => {
         const config = utils.pepConfig("prints.addCustomMessage");
         assert.strictEqual(typeof config, "string");
+    });
+
+    test("multipleStatements type", () => {
+        const config = utils.pepConfig("multipleStatements");
+        assert.strictEqual(typeof config, "boolean");
+    });
+
+    test("prints.includeParentCall type", () => {
+        const config = utils.pepConfig("hover.includeParentCall");
+        assert.strictEqual(typeof config, "boolean");
+    });
+
+    test("hover.includeParentheses type", () => {
+        const config = utils.pepConfig("hover.includeParentheses");
+        assert.strictEqual(typeof config, "boolean");
     });
 
     test("Test settings invalid configuration name", () => {
@@ -23,7 +53,7 @@ suite("Extension Settings", () => {
         }, Error);
     });
 
-    test("Test settings name", async () => {
+    test("Test settings update", async () => {
         await testUtils.updateConfig("prints.addCustomMessage", "test1");
         const config = utils.pepConfig("prints.addCustomMessage");
         assert.strictEqual(config, "test1");
@@ -54,5 +84,71 @@ suite("Misc", () => {
             editor.document.getText(),
             "# coding: utf-8\nfrom __future__ import print_function\n\ntest\n\n"
         );
+    });
+});
+
+suite("Commands name", () => {
+    const commands = testUtils.packageCommands();
+
+    test("Print Commands", () => {
+        for (const command of Object.values(printCommands)) {
+            assert.ok(commands.includes(command), `Extension command name mismatched: ${command}`);
+        }
+    });
+
+    test("Log Commands", () => {
+        for (const command of Object.values(logCommands)) {
+            assert.ok(commands.includes(command), `Extension command name mismatched: ${command}`);
+        }
+    });
+
+    test("Document Commands", () => {
+        for (const command of Object.values(documentCommands)) {
+            assert.ok(commands.includes(command), `Extension command name mismatched: ${command}`);
+        }
+    });
+});
+
+suite("Configuration names", () => {
+    /**
+     * Parse a directory for its files and check if has any configuration names.
+     *
+     * Check if configuration names match the one from the package.json.
+     *
+     * @param source string like path to parse for files
+     */
+    function parseDir(source: string) {
+        const configurations = testUtils.packageConfigurations();
+        readdirSync(source, { withFileTypes: true })
+            .filter((dirent) => dirent.isFile())
+            .map((dirent) => dirent.name)
+            .forEach((file) => {
+                const _file = path.join(source, file);
+                const fileContents = readFileSync(_file, "utf8");
+
+                const configs = fileContents.match(/(?<=pepConfig\(")(.+?)(?=")/g);
+                if (configs) {
+                    for (const config of configs) {
+                        // I test a wrong configuration so will certainly fail
+                        if (config === "randomTest") {
+                            continue;
+                        }
+                        assert.ok(
+                            configurations.includes(config),
+                            `file: ${file} - configuration name mismatch: ${config}`
+                        );
+                    }
+                }
+            });
+    }
+
+    test("In src files", () => {
+        const _path = path.join(testUtils.root, "src");
+        parseDir(_path);
+    });
+
+    test("In tests files", () => {
+        const _path = path.join(testUtils.root, "src", "test", "suite");
+        parseDir(_path);
     });
 });
