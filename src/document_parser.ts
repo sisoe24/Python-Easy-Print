3bb3b756e9618f0c3de33b2014bd4203a09adde4
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-01-26 11:57:26 -0500

    Add new commands jump to print

--- src/document_parser.ts
+++ src/document_parser.ts
@@ -81,6 +81,74 @@ export function deleteLines(editor: vscode.TextEditor): void {
     });
 }
 
+enum Direction {
+    up,
+    down,
+}
+
+/**
+ * Move the cursor at a specific print location.
+ *
+ * @param lines An array of LineObject to iterate over. If the direction is up,
+ * then the array must be reversed before.
+ * @param editor vscode active text editor
+ * @param direction A Direction enum value to indicate the search direction.
+ * @returns
+ */
+function jumpToPrint(
+    lines: Array<LineObject>,
+    editor: vscode.TextEditor,
+    direction: Direction
+): boolean {
+    for (const line of lines) {
+        // Decide the line comparison operator based on the command direction
+        let condition = line.range.start.line > editor.selection.start.line;
+        if (direction === Direction.up) {
+            condition = line.range.start.line < editor.selection.start.line;
+        }
+
+        if (condition) {
+            editor.selection = new vscode.Selection(line.range.start, line.range.start);
+            editor.revealRange(line.range);
+            return false;
+        }
+    }
+    return true;
+}
+
+/**
+ * Move cursor to Previous print made by the extension.
+ *
+ * If no prints are available, then place the cursor at the end of the file and
+ * try again.
+ *
+ * @param editor vscode active text editor
+ */
+export function jumpPrintPrevious(editor: vscode.TextEditor): void {
+    const endOfFile = jumpToPrint(documentParser(editor).reverse(), editor, Direction.up);
+    if (endOfFile) {
+        const lastLine = editor.document.lineCount;
+        editor.selection = new vscode.Selection(lastLine, 0, lastLine, 0);
+        jumpToPrint(documentParser(editor).reverse(), editor, Direction.up);
+    }
+}
+
+/**
+ * Move cursor to Next print made by the extension.
+ *
+ * If no prints are available, then place the cursor at the start of the file and
+ * try again.
+ *
+ * @param editor vscode active text editor
+ */
+export function jumpPrintNext(editor: vscode.TextEditor): void {
+    const endOfFile = jumpToPrint(documentParser(editor), editor, Direction.down);
+    if (endOfFile) {
+        editor.selection = new vscode.Selection(0, 0, 0, 0);
+        jumpToPrint(documentParser(editor), editor, Direction.down);
+    }
+}
+
 /**
  * Execute the specified command.
  *
@@ -104,6 +172,12 @@ export function executeCommand(action: string): void {
             break;
         case "delete":
             deleteLines(editor);
+            break;
+        case "jumpPrevious":
+            jumpPrintPrevious(editor);
+            break;
+        case "jumpNext":
+            jumpPrintNext(editor);
             break;
         default:
             throw new Error(`Command: ${action} not implemented`);
