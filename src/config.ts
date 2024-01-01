92d56ee6463a36e4f2e06b0105f0a3c47382b3c3
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-31 21:55:09 -0500

    Refactor config.ts file

--- src/config.ts
+++ src/config.ts
@@ -8,28 +8,34 @@ import * as vscode from "vscode";
  * @param property - name of the configuration property to get.
  * @returns - the value of the property.
  */
-export function getConfig(property: string): unknown {
+export function getConfig(property: string, missing?: unknown): unknown {
     const config = vscode.workspace.getConfiguration("pythonEasyPrint");
     const subConfig = config.get(property);
 
     if (typeof subConfig === "undefined") {
+        if (missing) {
+            console.warn(`Configuration: ${property} doesn't exist`);
+            return missing;
+        }
         throw new Error(`Configuration: ${property} doesn't exist`);
     }
 
     return subConfig;
 }
 
-export function symbol(): string {
+export function getSymbol(): string {
     const customSymbol = getConfig("prints.customSymbol") as string;
     return customSymbol || "➡";
 }
 
-export function logger(): string {
+export function getLogger(): string {
     const customLogger = getConfig("logging.customLogName") as string;
     return customLogger || "logging";
 }
 
 export function getCustomMessage(): string {
     const customMessage = getConfig("prints.customStatement") as string;
-    return customMessage || "print('─' * 50, '\\n┌─ %w:%l - {text}\\n└─', {text})";
+    return (
+        customMessage || "print('─' * 50, '\\n┌─ %w:%l - {text}\\n└─', {text})"
+    );
 }
