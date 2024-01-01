bb6d6fdc6a8871f41e894b049ae8bb1f15a83fd9
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-31 21:48:36 -0500

    Refactor PrintConstructor class and add new method for converting placeholders

--- src/print_constructor.ts
+++ src/print_constructor.ts
@@ -3,18 +3,26 @@ import * as path from "path";
 
 import * as config from "./config";
 
+
 /**
  * Convert placeholders symbols from the configuration settings.
  */
 export class PlaceholdersConverter {
     private editor;
+    public statement: string;
 
     /**
      * Init method to initialize the class.
      *
      * @param editor vscode active text editor
      */
-    constructor(editor: vscode.TextEditor) {
+    constructor(statement: string) {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            throw new Error("No active text editor");
+        }
+
+        this.statement = statement;
         this.editor = editor;
     }
 
@@ -61,7 +69,8 @@ export class PlaceholdersConverter {
                 continue;
             }
 
-            const currentLineIndentation = line.firstNonWhitespaceCharacterIndex;
+            const currentLineIndentation =
+                line.firstNonWhitespaceCharacterIndex;
 
             const match = /def\s(\w+)\(.*\):/.exec(line.text);
             if (match && startLineIndentation > currentLineIndentation) {
@@ -92,30 +101,14 @@ export class PlaceholdersConverter {
      * `%f`: filename, `%F`: function name, `%l`: line number.
      * @returns the converted placeholder.
      */
-    convert(key: string): string {
+    private convertPlaceholder(key: string): string {
         const placeholders: { [key: string]: string } = {
             "%f": this.getFilename(),
             "%F": this.getFuncName(),
             "%l": this.getLineNum(),
             "%w": this.getWorkspacePath(),
         };
-
-        if (!Object.prototype.hasOwnProperty.call(placeholders, key)) {
-            throw new Error(`Invalid placeholder type: ${key}`);
-        }
-
         return placeholders[key];
-    }
-}
-
-/**
- * Print Constructor class for the prints commands.
- */
-export class PrintConstructor {
-    private statement: string;
-
-    constructor(statement: string) {
-        this.statement = statement;
     }
 
     /**
@@ -126,8 +119,8 @@ export class PrintConstructor {
      *
      * @returns the converted placeholders
      */
-    convertPlaceholders(): string {
-        let customMsg: string;
+    convert(): string {
+        let customMsg = "";
 
         if (this.statement === "{@}") {
             customMsg = config.getCustomMessage();
@@ -135,74 +128,65 @@ export class PrintConstructor {
             customMsg = config.getConfig("prints.addCustomMessage") as string;
         }
 
-        // todo: would like to make this automated when adding a new placeholder
         const placeholderMatch = customMsg.match(/%[flFw]/g);
-
-        // TODO: maybe should pass the editor from executeCommand
-        const editor = vscode.window.activeTextEditor;
-        if (!placeholderMatch || !editor) {
+        if (!placeholderMatch) {
             return customMsg;
         }
 
-        const placeholders = new PlaceholdersConverter(editor);
         placeholderMatch.forEach((placeholder) => {
             customMsg = customMsg.replace(
                 placeholder,
-                placeholders.convert(placeholder)
+                this.convertPlaceholder(placeholder)
             );
         });
 
         return customMsg;
     }
-
-    /**
-     * Get the string statement for the print command.
-     *
-     * The statement could be a print statement or a logging statement based on the
-     * statement type. Placeholders are also converted if present.
-     *
-     * @returns the template statement: `print("➡ {text} :", {text})`
-     */
-    string(): string {
-        let s = "";
-
-        // statement is a logging statement
-        if (this.statement.includes("{logger}")) {
-            s = this.statement.replace("{logger}", config.logger());
-
-            if (config.getConfig("logging.useRepr")) {
-                s = s.replace("{#text}", "repr({text})");
-            } else {
-                s = s.replace("{#text}", "{text}");
-            }
-        } else {
-            const placeholders = this.convertPlaceholders();
-
-            // when no placeholders are present, we need to replace the {@} with a space
-            const replaceToken = placeholders ? "{@}" : "{@} ";
-
-            s = this.statement
-                .replace(replaceToken, placeholders)
-                .replace("{symbol}", config.symbol());
-
-            if (config.getConfig("prints.printToNewLine")) {
-                s = s.split(":'").join(":\\n'");
-            }
-        }
-
-        return s;
-    }
 }
 
 /**
- * Construct the template statement for the command.
+ * Get the string statement for the print command.
  *
- * Based on which statement is supplied as argument, the function will return either
- * a print statement or a log statement.
+ * The statement could be a print statement or a logging statement based on the
+ * statement type. Placeholders are also converted if present.
  *
- * @param statementType name of the statement to get. Could be log or prints
- * @returns the template statement (`print("➡ {text} :", {text})`)
+ * @returns the template statement: `print("➡ {text} :", {text})`
  */
-export function printConstructor(statement: string): string {
-    return new PrintConstructor(statement).string();
+export function printConstructorWithPlaceholders(
+    statement: string,
+    placeholders: string,
+): string {
+    let s = "";
+
+    // statement is a logging statement
+    if (statement.includes("{logger}")) {
+        s = statement.replace("{logger}", config.logger());
+
+        if (config.getConfig("logging.useRepr")) {
+            s = s.replace("{#text}", "repr({text})");
+        } else {
+            s = s.replace("{#text}", "{text}");
+        }
+    } else {
+        // when no placeholders are present, we need to replace the {@} with a space
+        const replaceToken = placeholders ? "{@}" : "{@} ";
+
+        s = statement
+            .replace(replaceToken, placeholders)
+            .replace("{symbol}", config.symbol());
+
+        if (config.getConfig("prints.printToNewLine")) {
+            s = s.split(":'").join(":\\n'");
+        }
+    }
+
+    return s;
+}
+
+export function printConstructor(statement: string) {
+    const placeholders = new PlaceholdersConverter(statement);
+    return printConstructorWithPlaceholders(
+        statement,
+        placeholders.convert()
+    );
 }
